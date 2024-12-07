e1d2454d85f7313b2a38ec81efa7d314b950712f
Pratik <dhanepratik543@gmail.com>
2024-12-07 14:18:48 +0530

    minor changes

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,8 +1,7 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
-//const db = process.env.DATABASE;
+const db = process.env.DATABASE;
 
-const db = 'mongodb+srv://dhanepratik543:ODXuIu9a5nEhzxIm@elderease.u0jv9nd.mongodb.net/';
 
 mongoose
   .connect(db)
